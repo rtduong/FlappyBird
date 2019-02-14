4b8b57e668c1579a5f42e788dc0f4b56517c9eca
Raymond Duong <raymond.duong@avanade.com>
2019-02-13 17:10:03 -0800

    Add input box and button with event handlers

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: ''
+    }
+    handleLocationInput = (text) => {
+      // TODO: Autocomplete - saved searches and/or autocomplete
+      alert('Handling Location Input');
+      this.state.location = text;
+    }
+    search = (text) => {
+      // TODO: Call API
+      alert('You searched for ' + text)
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+
+        <View style={styles.flexlocationInput}>
+          <TextInput style={styles.locationInput}
+            placeholder="QAZIP Code"
+            onChangeText={(text) => handleLocationInput(text)}
+          />
+
+          <Button style={styles.searchButton}
+            title="S"
+            onPress={() => {
+              // TODO: Change button to use TouchableOpacity for more styling options
+              alert('You tapped the button!');
+              search(this.state.location);
+            }}
+          />
+        </View>
+
+        <View>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+
       </View>
     );
   }
@@ -14,8 +51,23 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    padding: 40
   },
+  flexlocationInput: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'center'
+  },
+  locationInput: {
+    height: 40,
+    borderColor: '#7a42f4',
+    borderWidth: 1
+  },
+  searchButton: {
+    color: "#90A4AE",
+    width: 40,
+    height: 40
+  }
 });
